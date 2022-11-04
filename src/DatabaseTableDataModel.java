d7e321360fbf5fb959d86935a9da8ea569a3c3ab
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-04 18:12:13 +0800

    a simple DatabaseTableDataModel

---
+++ src/DatabaseTableDataModel.java
@@ -0,0 +1,25 @@
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+public class DatabaseTableDataModel extends AbstractTableModel {
+    ArrayList<String[]> data;
+
+    public DatabaseTableDataModel(String table_name) {
+        data = Fetch.FetchAllRows(table_name);
+    }
+
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return data.get(0).length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return data.get(rowIndex)[columnIndex];
+    }
+}
