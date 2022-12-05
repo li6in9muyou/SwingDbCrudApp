bbc2422f9a3fca0bbc51d422bb1377276cdd8e81
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-12-05 23:19:51 +0800

    fix: scrolling large pictures feels laggy

--- src/HighlightNullAndEmptyString.java
+++ src/HighlightNullAndEmptyString.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
+import java.util.HashMap;
 
 public class HighlightNullAndEmptyString extends DefaultTableCellRenderer implements TableCellRenderer {
     private final TableCellRenderer defaultRenderer;
+    private final HashMap<Integer, ImageIcon> cachedImage = new HashMap<>(10);
 
     public HighlightNullAndEmptyString(TableCellRenderer defaultRenderer) {
         this.defaultRenderer = defaultRenderer;
@@ -27,11 +29,18 @@ public class HighlightNullAndEmptyString extends DefaultTableCellRenderer implem
             JLabel base = (JLabel) defaultRenderer.getTableCellRendererComponent(
                     table, "", isSelected, hasFocus, row, column
             );
-            Image image = new ImageIcon((byte[]) value).getImage();
-            float ar = (float) image.getWidth(null) / (float) image.getHeight(null);
-            base.setIcon(new ImageIcon(
-                    image.getScaledInstance((int) (300 * ar), 300, Image.SCALE_DEFAULT)
-            ));
+            int key = value.hashCode();
+            if (!cachedImage.containsKey(key)) {
+                Image image = new ImageIcon((byte[]) value).getImage();
+                float ar = (float) image.getWidth(null) / (float) image.getHeight(null);
+                cachedImage.put(
+                        key,
+                        new ImageIcon(image.getScaledInstance(
+                                (int) (300 * ar), 300, Image.SCALE_DEFAULT
+                        ))
+                );
+            }
+            base.setIcon(cachedImage.get(key));
             return base;
         } else {
             return defaultRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
